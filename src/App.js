8fc887b3218733d946c3ba58a8304c0a101c4a04
bgarcia95 <bgarcia.95@outlook.com>
2019-10-08 15:37:38 -0600

    Agregando Formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
 import CategoriasProvider from './context/CategoriasContext';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <CategoriasProvider>
       <Header />
+
+      <div className="uk-container">
+        <Formulario />
+      </div>
+
     </CategoriasProvider>
   );
 }
